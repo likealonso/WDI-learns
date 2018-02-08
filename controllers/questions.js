c34b9616214a5d51f166e1ca1f3852004efde1f2
Alonso Tacanga <alonso.tacanga@gmail.com>
2018-02-08 14:18:10 -0800

    cleanup

--- controllers/questions.js
+++ controllers/questions.js
@@ -7,6 +7,5 @@ function index (req, res) {
 
 module.exports = {
     index
-    
 }
 
--- seed/seeds.js
+++ seed/seeds.js
@@ -3,9 +3,7 @@ require('./../config/database');
 const mongoose = require('mongoose');
 const sample = require('./data');
 const Unit = require('../models/unit');
-// const Question = require('../models/question');
 const sood = Unit.remove({})
-// const seed = Question.remove({})
 
 sood.then(() => {
     return Unit.create(sample)
@@ -18,28 +16,8 @@ sood.then(() => {
 })
 
 .catch(err => {
-    console.log('\n\n\nERROR!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-    console.log('err =', err);
     process.exit();
 })
 
 
 .catch(error => console.log(error))
-
-//create questions
-// seed.then(()=> {
-//     return Question.create(sample)
-    // Question.create({unitId:2})
-    // .then(q => {
-    //     q.choices.push({text:"do you like flowers?", choiceId: 'a', correct: false});
-    //     q.save()
-    //     return q
-    // })
-    // console.log('creatin qs')
-// })
-
-// .then((question) => {
-//     console.log(question);
-//     mongoose.connection.close();
-//     process.exit();
-// })
