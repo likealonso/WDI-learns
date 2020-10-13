730188c729601fafced17132ccdfef6f3afd870d
Alonso Tacanga <alonso.tacanga@gmail.com>
2020-10-12 22:12:08 -0700

    right pass

--- config/database.js
+++ config/database.js
@@ -2,8 +2,9 @@ const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
 // mongoose.connect("mongodb://localhost/wdi-learns");
-mongoose.connect(process.env.DATABASE_URL);
+//mongoose.connect(process.env.DATABASE_URL);
 // mongoose.connect('mongodb://localhost/amaz');
+mongoose.connect("mongodb+srv://new-user:abc123abc123@wdi-learns.geijo.azure.mongodb.net/wdi-learns?retryWrites=true&w=majority")
 
 const db = mongoose.connection;
 
