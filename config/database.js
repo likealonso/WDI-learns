2d7f646a362264e9048a0096d98a1fd4de5785bc
Alonso Tacanga <alonso.tacanga@gmail.com>
2020-10-12 17:19:56 -0700

    atlas db change 2

--- config/database.js
+++ config/database.js
@@ -3,15 +3,7 @@ mongoose.Promise = global.Promise;
 
 // mongoose.connect("mongodb://localhost/wdi-learns");
 //mongoose.connect(process.env.DATABASE_URL);
-const uri = "mongodb+srv://alonso:haulass21@wdi-learns.geijo.azure.mongodb.net/wdi-learns?retryWrites=true&w=majority"
-mongoose.connect(uri,{
-    useNewUrlParser:true,
-    useUnifiedTopology:true
-})
-.then(() => {
-    console.log('MongoDB Connected')
-})
-.catch(err=> console.log(err))
+mongoose.connect("mongodb+srv://alonso:haulass21@wdi-learns.geijo.azure.mongodb.net/wdi-learns?retryWrites=true&w=majority")
 // mongoose.connect('mongodb://localhost/amaz');
 
 const db = mongoose.connection;
