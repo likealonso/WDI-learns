a2d2581f81f7aa3f953587a1b0a7c590a5251831
Alonso Tacanga <alonso.tacanga@gmail.com>
2020-10-12 19:25:55 -0700

    console again 4

--- config/database.js
+++ config/database.js
@@ -1,11 +1,20 @@
-const mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-console.log('are we hitting this?')
+/*const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;*/
 
 // mongoose.connect("mongodb://localhost/wdi-learns");
-mongoose.connect(process.env.DATABASE_URL);
+//mongoose.connect(process.env.DATABASE_URL);
 // mongoose.connect('mongodb://localhost/amaz');
 
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb+srv://alonso:haulass21@wdi-learns.geijo.azure.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+/*
 const db = mongoose.connection;
 
 db.once('open', () => {
@@ -14,4 +23,4 @@ db.once('open', () => {
 
 db.on('error', (err) => {
     console.error(`Database error: \n${err}`)
-})
+})*/
